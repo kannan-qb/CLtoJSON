fe0cc8c69847f7043370d6e0168ee257b65ca382
kannan-qb <kannan.r@qbrainx.com>
2021-08-29 20:19:33 +0530

    Assignment

--- src/main/scala/com/qbrainx/util/JsonFormatter.scala
+++ src/main/scala/com/qbrainx/util/JsonFormatter.scala
@@ -6,23 +6,23 @@ import scala.annotation.tailrec
 
 object JsonFormatter extends App {
   @tailrec
-  def write(b: Int): Unit = {
+  def writeJson(b: Int): Unit = {
     val input = io.StdIn.readLine()
     val firstChar: Char = input.charAt(0)
     firstChar match {
       case 'A' => val a = listMaker(input)
                   jsonConversion(a(1))
-                  write(b+1)
+                  writeJson(b+1)
       case 'R' =>  arrayofJson match {
         case JsArray.empty  => println("JSON Empty")
         case _ => println(arrayofJson.prettyPrint)
       }
                    arrayofJson = JsArray.empty
                    values = Vector.empty
-                   write(b+1)
+                   writeJson(b+1)
       case _ => println("INVALID INPUT! TRY AGAIN")
-                write(b+1)
+                writeJson(b+1)
     }
   }
-  write(0)
+  writeJson(0)
 }
